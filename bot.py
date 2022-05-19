9457731dbf82fa34a63b52c9ba6045331053178d
marty321 <redish361@gmail.com>
2022-05-19 14:15:40 +0100

    rebasin

--- bot.py
+++ bot.py
@@ -3,15 +3,24 @@ import os
 
 client = discord.Client()
 
+PREFIX = ".."
+TOKEN = "token goes here"
+
 @client.event
 async def on_ready():
- print('We have logged in as {0.user}'.format(client))
+   print('We have logged in as {0.user}'.format(client))
+   
 @client.event
 async def on_message(message):
- if message.content.startswith('..fish'):
-  await message.channel.send('Fishing successful!')
-client.run(os.getenv('hello'))
+    if not message.content.startswith(PREFIX):
+        return
+    command = message.content[len(prefix):]
+    
+    if command == 'fish'
+      await message.channel.send('Fishing successful!')
 
- if message.content.startswith('..farm'):
-  await message.channel.send('Farming successful!')
-client.run(os.getenv('hello'))
+    if command == '..farm':
+      await message.channel.send('Farming successful!')
+
+
+client.run(TOKEN)
