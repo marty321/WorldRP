e27fe8312b5faebba6f5d822ffd4d214e74f3437
marty321 <redish361@gmail.com>
2022-05-22 10:01:34 +0100

    adding .gitignore

--- bot.py
+++ bot.py
@@ -7,18 +7,15 @@ PREFIX = ".."
 
 tokenFile = open("token.txt","r")
 TOKEN = tokenFile.read()
+tokenFile.close()
+print(TOKEN)
 if TOKEN == "":
   raise RuntimeError("No token in token.txt")
-tokenFile.close()
+
 
 @client.event
 async def on_ready():
    print('We have logged in as {0.user}'.format(client))
-   
-@client.event
-async def on_message(message):
-    if not message.content.startswith(PREFIX):
-        return
-    command = message.content[len(PREFIX):]
+
 
 client.run(TOKEN)
